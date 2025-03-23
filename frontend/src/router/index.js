315e694b2ca4acbc1933a80ee4b2c72e8339c1a9
Lívia De Araújo Nunes <liviaaraujonunes@gmail.com>
2025-03-23 20:21:54 -0300

    chore: tela de login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Register from '../views/Register.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
-    path: '/register',
-    name: 'register',
-    component: Register
+    path: "/register",
+    name: "register",
+    component: Register,
   },
-]
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="grid grid-cols-2 h-[100vh] w-[100vw]">
+    <div
+      class="bg-red-500 bg-center bg-cover bg-[url(@/assets/airplane.png)]"
+    ></div>
+    <div class="flex justify-center items-center">
+      <div class="mb-3 h-[60%] w-[70%] rounded rounded-lg p-6">
+        <p class="mb-6 text-4xl font-semibold text-blue-800">Entrar</p>
+        <Input
+          title="Email"
+          type="email"
+          v-model="object.email"
+          required
+          :error="errors.email"
+        />
+        <Input
+          title="Senha"
+          type="password"
+          v-model="object.password"
+          required
+          :error="errors.password"
+        />
+        <Button blue label="Entrar" @click="register()" />
+        <div class="flex justify-end mt-6"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from "../components/Input.vue";
+import Button from "../components/Button.vue";
+
+export default {
+  components: {
+    Input,
+    Button,
+  },
+  data: () => ({
+    object: {},
+    errors: {},
+  }),
+  methods: {
+    async register() {
+      if (this.object.email && this.object.name && this.object.password) {
+        // const response = await this.register(this.object) //implementar depois no backend
+        // if (!response?.data) {
+        // }
+        this.$router.push("login");
+      } else {
+        if (!this.object.name) this.errors.name = "Nome é obrigatório";
+        if (!this.object.email) this.errors.email = "Email é obrigatório";
+        if (!this.object.password) this.errors.password = "Senha é obrigatória";
+      }
+    },
+  },
+};
+</script>
