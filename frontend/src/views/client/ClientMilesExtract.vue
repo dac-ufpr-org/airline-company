162d8ddba4fdbec43964253f1262c1176c8a9c7e
CamilaVerssao <camila.verssao@ufpr.br>
2025-03-28 08:37:26 -0300

    feat: inicio criação de extrato de milhas do cliente

---
+++ frontend/src/views/client/ClientMilesExtract.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import FlightsTable from '../../components/FlightsTable.vue'
+
+export default {
+    components: {
+        FlightsTable
+    }
+}
+</script>
