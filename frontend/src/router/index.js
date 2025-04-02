4167f2a8a456b2257db96d81221b321e3d06bdb4
Lívia de Araújo Nunes <liviaaraujonunes@gmail.com>
2025-04-02 17:51:05 -0300

    feat: rota de listagem de funcionários

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,33 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Register from "../views/Register.vue";
-import Login from "../views/Login.vue";
-import ClientDashboard from "../views/client/ClientDashboard.vue";
-import ClientMilesExtract from "../views/client/ClientMilesExtract.vue";
-import EmployeeDashboard from "@/views/employee/EmployeeDashboard.vue";
-import NewReservation from "@/views/client/NewReservation.vue";
-import ReservationConfirmation from "@/views/client/ReservationConfirmation.vue";
-import ReservationReceipt from "@/views/client/ReservationReceipt.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
+import ClientDashboard from '../views/client/ClientDashboard.vue'
+import ClientMilesExtract from '../views/client/ClientMilesExtract.vue'
+import EmployeeDashboard from '@/views/employee/EmployeeDashboard.vue'
+import NewReservation from '@/views/client/NewReservation.vue'
+import ReservationConfirmation from '@/views/client/ReservationConfirmation.vue'
+import ReservationReceipt from '@/views/client/ReservationReceipt.vue'
+import Employee from '@/views/employee/Employee.vue'
 
 const routes = [
   {
-    path: "/register",
-    name: "register",
+    path: '/register',
+    name: 'register',
     component: Register,
   },
   {
-    path: "/login",
-    name: "login",
+    path: '/login',
+    name: 'login',
     component: Login,
   },
   //client routes
-  { path: '/client/dashboard', 
-    name: "Dashboard do Cliente",
-    component: ClientDashboard 
-  },
-  { path: '/client/mileage-extract', 
-    name: "Extrato de Milhas",
-    component: ClientMilesExtract 
-  },
+  { path: '/client/dashboard', name: 'Dashboard do Cliente', component: ClientDashboard },
+  { path: '/client/mileage-extract', name: 'Extrato de Milhas', component: ClientMilesExtract },
   {
     path: '/client/reservas/nova',
     name: 'Nova Reserva',
@@ -44,15 +39,14 @@ const routes = [
     component: ReservationReceipt,
   },
   //employee routes
-  { path: '/employee/dashboard', 
-    name: "Dashboard do Funcionário",
-    component: EmployeeDashboard
-  },
-];
+  { path: '/employee/dashboard', name: 'Dashboard do Funcionário', component: EmployeeDashboard },
+
+  { path: '/employee', name: 'Funcionários', component: Employee },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
