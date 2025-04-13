4bd4c1ae292f649c15b97de9054a43283bab9702
Camila <camila.verssao@gmail.com>
2025-04-13 20:30:35 -0300

    feat: template ms voo

---
+++ ms-voo/src/main/java/com/msvoo/ms_voo/MsVooApplication.java
@@ -0,0 +1,13 @@
+package com.msvoo.ms_voo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVooApplication.class, args);
+	}
+
+}
---
+++ ms-voo/src/test/java/com/msvoo/ms_voo/MsVooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvoo.ms_voo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
