ebb6ab228deed769b04104a795e6efdafe3a528e
Lívia De Araújo Nunes <liviaaraujonunes@gmail.com>
2025-05-06 23:49:05 -0300

    chore: ajuste na componente da tabela

--- frontend/src/components/general/Table.vue
+++ frontend/src/components/general/Table.vue
@@ -1,13 +1,16 @@
 <template>
   <main class="flex w-full h-full">
     <!-- Condicionalmente exibe a barra lateral de navegação -->
-    <TableSidebar 
+    <TableSidebar
       v-if="activeSidebar"
       :active-tab="activeTab"
       @tab-change="handleTabChange"
     />
 
-    <div :class="!activeSidebar ? 'ml-4' : ''" class="overflow-x-auto bg-white my-4 mr-4 w-full p-6 rounded-xl shadow-lg border border-blue-200">
+    <div
+      :class="!activeSidebar ? 'ml-4' : ''"
+      class="overflow-x-auto bg-white my-4 mr-4 w-full p-6 rounded-xl shadow-lg border border-blue-200"
+    >
       <!-- Espaço para ações de pesquisa -->
       <div class="flex gap-4"></div>
 
@@ -22,8 +25,11 @@
           <thead class="bg-gray-50">
             <tr>
               <!-- Loop sobre as colunas para renderizar os cabeçalhos -->
-              <th v-for="column in columns" :key="column.key" 
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th
+                v-for="column in columns"
+                :key="column.key"
+                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
                 {{ column.label }}
               </th>
             </tr>
@@ -33,10 +39,18 @@
           <tbody class="bg-white divide-y divide-gray-200">
             <tr v-for="(item, index) in filteredItems" :key="index">
               <!-- Loop sobre as colunas para renderizar as células -->
-              <td v-for="column in columns" :key="column.key" class="px-6 py-4 whitespace-nowrap">
+              <td
+                v-for="column in columns"
+                :key="column.key"
+                class="px-6 py-4 whitespace-nowrap"
+              >
                 <slot :name="`cell-${column.key}`" :item="item">
                   <!-- Formatação dos dados das células com base no tipo de coluna -->
-                  {{ column.formatter ? column.formatter(item[column.key]) : item[column.key] }}
+                  {{
+                    column.formatter
+                      ? column.formatter(item[column.key])
+                      : item[column.key]
+                  }}
                 </slot>
               </td>
             </tr>
@@ -45,7 +59,10 @@
       </div>
 
       <!-- Mensagem caso não haja itens filtrados -->
-      <div v-if="filteredItems.length === 0" class="text-center py-8 text-gray-500">
+      <div
+        v-if="filteredItems.length === 0"
+        class="text-center py-8 text-gray-500"
+      >
         Nenhum registro encontrado.
       </div>
     </div>
@@ -58,76 +75,78 @@ export default {
     items: {
       type: Array,
       default: [],
-      required: true
+      required: true,
     },
     columns: {
       type: Array,
-      required: true
+      required: true,
     },
     initialSearchTerm: {
       type: String,
-      default: ''
+      default: "",
     },
     activeSidebar: {
       type: Boolean,
-      default: false
+      default: false,
     },
   },
   data() {
     return {
       searchTerm: this.initialSearchTerm,
-      activeTab: 'reservas'
-    }
+      activeTab: "reservas",
+    };
   },
   computed: {
-    // Computa os itens filtrados com base no termo de pesquisa
     filteredItems() {
-      // Verifica se existem itens
       if (this.items.length) {
-        // Se não houver termo de pesquisa, retorna todos os itens
-        if (!this.searchTerm) return this.items
+        if (!this.searchTerm) return this.items;
 
-        const term = this.searchTerm.toLowerCase()
+        const term = this.searchTerm.toLowerCase();
 
-        // Filtra os itens com base nas colunas e no termo de pesquisa
-        return this.items.filter(item => 
-          this.columns.some(column => {
-            const value = column.formatter 
-              ? column.formatter(item[column.key]) 
-              : item[column.key]
-            // Verifica se o valor da célula contém o termo de pesquisa
-            return String(value).toLowerCase().includes(term)
-          }) 
-        )
+        return this.items.filter((item) =>
+          this.columns.some((column) => {
+            const value = column.formatter
+              ? column.formatter(item[column.key])
+              : item[column.key];
+            return String(value).toLowerCase().includes(term);
+          })
+        );
       }
-      return []
+      return [];
     },
 
-    // Condicionalmente adiciona a coluna de "Ações" dependendo da aba ativa
-    columns() {
+    reservationColumns() {
       const columns = [
-        { key: 'dataHora', label: 'Data/Hora', formatter: (val) => this.$filters.formatDateTime(val) ?? '-' },
-        { key: 'origem', label: 'Origem' },
-        { key: 'destino', label: 'Destino' },
-        { key: 'milhas', label: 'Milhas', formatter: (val) => `${val} milhas` ?? '-' },
-        { key: 'status', label: 'Status' },
-      ]
+        {
+          key: "dataHora",
+          label: "Data/Hora",
+          formatter: (val) => this.$filters.formatDateTime(val) ?? "-",
+        },
+        { key: "origem", label: "Origem" },
+        { key: "destino", label: "Destino" },
+        {
+          key: "milhas",
+          label: "Milhas",
+          formatter: (val) => `${val} milhas` ?? "-",
+        },
+        { key: "status", label: "Status" },
+      ];
 
-      // Adiciona a coluna "Ações" apenas se a aba ativa for "minhas reservas"
-      if (this.activeTab === 'reservas') {
-        columns.push({ key: 'actions', label: 'Ações', class: 'text-right' })
+      if (this.activeTab === "reservas") {
+        columns.push({ key: "actions", label: "Ações", class: "text-right" });
       }
 
       return columns;
-    }
+    },
   },
+
   methods: {
     // Método para gerenciar a troca de abas
-    handleTabChange(newTab){
-      this.activeTab = newTab
+    handleTabChange(newTab) {
+      this.activeTab = newTab;
       // Emite o evento de troca de aba
-      this.$emit('tab-change', newTab)
-    }
-  }
-}
+      this.$emit("tab-change", newTab);
+    },
+  },
+};
 </script>
