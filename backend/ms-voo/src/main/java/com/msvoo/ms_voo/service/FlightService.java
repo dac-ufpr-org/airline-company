491724f8bc7529be95caf878b0596ec38a1ff8b3
Lívia De Araújo Nunes <liviaaraujonunes@gmail.com>
2025-05-15 17:28:19 -0300

    feat: controller and flight service

---
+++ backend/ms-voo/src/main/java/com/msvoo/ms_voo/service/FlightService.java
@@ -0,0 +1,21 @@
+package main.java.com.msvoo.ms_voo.service;
+import com.msvoo.ms_voo.entity.Flight;
+import org.springframework.stereotype.Service;
+
+
+@Service
+
+public class FlightService {
+ 
+
+    public Flight createFlight(Flight flight) {
+        //  Lógica para salvar o voo no banco de dados
+        return flight; 
+    }
+    public Flight getFlightById(Long id) {
+        //  Lógica para recuperar o voo pelo ID do banco de dados
+        return null; 
+    }
+}
+
+
