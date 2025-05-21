4687389e0b28f4eebfbfafd33b598135a2785eca
Natasha Alcaide <natasha.as@hotmail.com>
2025-05-21 18:31:37 -0300

    feat: implementa entidade Reservation, status e repositorio

---
+++ backend/ms-reserva/src/main/java/com/msreserva/ms_reserva/model/Reservation.java
@@ -0,0 +1,69 @@
+package com.msreserva.ms_reserva.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Reservation {
+    
+        @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long clientId;
+    private String flightCode;
+
+    private LocalDate reservationDate;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
+    public Reservation() {}
+
+    public Reservation(Long clientId, String flightCode, LocalDate reservationDate, ReservationStatus status) {
+        this.clientId = clientId;
+        this.flightCode = flightCode;
+        this.reservationDate = reservationDate;
+        this.status = status;
+    }
+
+    // GETTERS E SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public void setFlightCode(String flightCode) {
+        this.flightCode = flightCode;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+}
+    
+
---
+++ backend/ms-reserva/src/main/java/com/msreserva/ms_reserva/model/ReservationStatus.java
@@ -0,0 +1,12 @@
+package com.msreserva.ms_reserva.model;
+
+public enum ReservationStatus {
+    CRIADA,
+    CHECK_IN,
+    CANCELADA,
+    CANCELADA_VOO,
+    EMBARCADA,
+    REALIZADA,
+    NAO_REALIZADA
+    
+}
