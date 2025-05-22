49395337c29eb689dd0de1d163d71c9dd14aafca
Lívia De Araújo Nunes <liviaaraujonunes@gmail.com>
2025-05-21 22:05:22 -0300

    feat: dto do voo

---
+++ backend/ms-voo/src/main/java/com/msvoo/ms_voo/dto/Flightdto.java
@@ -0,0 +1,18 @@
+package main.java.com.msvoo.ms_voo.dto;
+
+import lombok.Data;
+
+
+@Data
+public class Flightdto {
+    private String flightDate;
+    private String flightTime;
+    private String origin;
+    private String destination;
+    private String flightNumber;
+    private String departure;
+    private String arrival;
+    private String status;
+    
+}
+
